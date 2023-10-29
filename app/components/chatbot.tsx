c3a852b5f63a80b918b8dbd7664a856ba96c62c1
caio myra pereira <caiomyrapereira@hotmail.com>
2023-10-29 00:53:45 -0300

    ajuste no chatbot.

--- app/components/chatbot.tsx
+++ app/components/chatbot.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 
+import Image from 'next/image'
 import './ChatStyle.css';
 import { useEffect, useState } from 'react';
 
@@ -11,8 +12,14 @@ import { useEffect, useState } from 'react';
 
 const Chatbot = () => {
 
+
     const [loading, setLoading] = useState(false);
-    const [data , setData] = useState(null);
+    // const [text, setText ] =  useState(null);
+    const [data, setData] = useState(null);
+    const [tags, setTags] = useState("");
+
+    const [artists, setArtists] = useState<string[]>([]);
+
 
 
     const fetchChat = async () => {
@@ -29,11 +36,13 @@ const Chatbot = () => {
             if (!data)
                 throw 'Problema na requisição';
 
-       
+
+
             if (data) {
                 setLoading(true)
                 console.log(data.chat)
                 setData(data.chat);
+                setArtists([...artists, ('robot: ' + data.chat)])
             }
 
 
@@ -41,18 +50,69 @@ const Chatbot = () => {
         } catch (error) {
             console.log(error)
         } finally {
-        
+
         }
     }
 
+    const fetchInteraction = async () => {
+        try {
+            const response = await fetch('http://127.0.0.1:8000/chatbot_api/chat', {
+                method: 'POST',
+                headers: new Headers({ 'Content-type': 'application/json' }),
+                body: JSON.stringify({
+                    name: "fdfdfdfdfdffdf",
+                    description: tags
+                }),
+                mode: 'cors'
+            });
 
+            const data = await response.json();
+
+            console.log('data mensagem', data);
+
+            if (!data)
+                throw 'Problema na requisição';
+
+            console.log(data.description)
+            setArtists([...artists, ('robot: ' + data.description)])
+
+
+
+
+
+
+
+        } catch (error) {
+            console.log(error)
+        } finally {
+
+        }
+
+    }
 
     useEffect(() => {
-        console.log('carai bixo')
-        fetchChat();
+        console.log('data2:', data)
+        if (!data) {
+            console.log('carai bixo')
+            fetchChat();
+        }
     }, [])
 
-    console.log('data:', data)
+    function closeChatbot() {
+        setLoading(false);
+    }
+
+    function abertChatbot() {
+        setLoading(true);
+    }
+
+    function sendMessageChatbot() {
+        setArtists([...artists, ('eu: ' + tags)])
+        setTags('');
+
+        fetchInteraction();
+    }
+
 
 
     return (
@@ -60,8 +120,24 @@ const Chatbot = () => {
             <div>
                 {
                     loading
-                        ? <div className='chatbotabert'>  {data} </div>
-                        : <div className='chatbotClose'  ></div>
+                        ? <div className='chatbotabert'>
+                            <div className='btnClose'><button onClick={closeChatbot}>X</button> </div>
+                            <div className="chatbotContent">
+                                {
+                                    artists.map( (artist, index) => (
+                                        <p key={index}>{artist}</p>
+                                    ))
+                                }
+
+                            </div>
+                            <div className='chatbotFrom'>
+                                <input name='tags' id="tags" placeholder='tags(, seperated)' value={tags} onChange={e => setTags(e.target.value)} />
+                                <button onClick={sendMessageChatbot}> enviar </button>
+                            </div>
+                        </div>
+                        : <div className='chatbotClose' onClick={abertChatbot}  >
+                            <Image src='/home/chat.png' alt='ESG Logo' width={65} height={20} priority />
+                        </div>
                 }
             </div>
         </>
@@ -71,3 +147,4 @@ const Chatbot = () => {
 
 
 export default Chatbot
+
